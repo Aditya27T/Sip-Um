2f139a73d6692b11d6cd8e60fe65550398d31122
aditya27t <aditpoetra25@gmail.com>
2026-08-13 14:52:56 +0700

    feat: add Docker Compose, Dockerfiles, and env template

--- apps/sip-fe/next.config.mjs
+++ apps/sip-fe/next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "sip-be",
+        port: "5000",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "5000",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
